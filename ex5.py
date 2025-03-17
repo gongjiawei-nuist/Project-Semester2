6d1bbaa693e7cf1d6971948f22049f845aac609f
gjw <bno03972365@163.com>
2025-03-17 19:30:26 +0800

    Add ex5.py ex6.py to repository

---
+++ ex5.py
@@ -0,0 +1,9 @@
+x=int(input("Please enter your number:"))
+res=1
+while(x>1):
+  
+  res=res*x
+  x=x-1
+print(res)  
+
+ 
---
+++ ex6.py
@@ -0,0 +1,6 @@
+x=int(input("Please enter your nunber"))
+res=1
+for i in range(1,x):
+  res=res*x
+  x=x-1
+print(res)
